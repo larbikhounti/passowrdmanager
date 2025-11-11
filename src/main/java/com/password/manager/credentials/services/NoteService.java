38cefd399c15f2c9a6e92b602aa6ed8bdd327e7a
mohamed khounti <larbikhounti@gmail.com>
2025-11-11 19:04:06 +0100

    add NoteService class for managing note operations

---
+++ src/main/java/com/password/manager/credentials/services/NoteService.java
@@ -0,0 +1,32 @@
+package com.password.manager.credentials.services;
+
+import com.password.manager.credentials.base.Entity;
+import com.password.manager.credentials.contracts.ICredential;
+
+public class NoteService implements ICredential {
+
+    @Override
+    public boolean addCredential(Entity credential) {
+        return false;
+    }
+
+    @Override
+    public boolean editCredential(int id, Entity credential) {
+        return false;
+    }
+
+    @Override
+    public boolean removeCredential(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean getCredential(int id) {
+        return false;
+    }
+
+    @Override
+    public Entity[] getAllCredentials() {
+        return new Entity[0];
+    }
+}
