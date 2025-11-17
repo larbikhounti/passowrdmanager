93d01b8510ef9f8e181621ee54dce4bde6dae27e
mohamed khounti <larbikhounti@gmail.com>
2025-11-17 16:42:47 +0100

    feat: implement credential storage and logging in CreditCardService

--- src/main/java/com/password/manager/credentials/services/CreditCardService.java
+++ src/main/java/com/password/manager/credentials/services/CreditCardService.java
@@ -6,9 +6,19 @@ import com.password.manager.credentials.contracts.ICredential;
 import java.util.ArrayList;
 
 public class CreditCardService implements ICredential {
+    private final ArrayList<Entity> _credentials;
+
+    public CreditCardService() {
+        _credentials = Entity.getCredentials();
+    }
     @Override
     public boolean addCredential(Entity credential) {
-        return false;
+        boolean result =  _credentials.add(credential);
+        for (Entity c : _credentials) {
+            System.out.printf("id is %d creditCard is %s creditCardHolderName is %s creditCardExpiry is %s creditCardCVV is %s \n",
+                    c.getId(), c.getcreditCardNumber(), c.getcreditCardHolderName(), c.getcreditCardExpiry(), c.getcreditCardCVV());
+        }
+        return result;
     }
 
     @Override
