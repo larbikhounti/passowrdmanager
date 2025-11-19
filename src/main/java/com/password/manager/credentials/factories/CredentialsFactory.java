2ed653e9017c9ada682f0190bbaab1ec69449fe0
mohamed khounti <larbikhounti@gmail.com>
2025-11-19 19:08:38 +0100

    feat: refactor CredentialsFactory to use Entity for credential service retrieval

--- src/main/java/com/password/manager/credentials/factories/CredentialsFactory.java
+++ src/main/java/com/password/manager/credentials/factories/CredentialsFactory.java
@@ -1,18 +1,20 @@
 package com.password.manager.credentials.factories;
 
 
+import com.password.manager.credentials.base.Entity;
 import com.password.manager.credentials.contracts.ICredential;
+import com.password.manager.credentials.enums.CredentialEnum;
 import com.password.manager.credentials.services.CreditCardService;
 import com.password.manager.credentials.services.EmailService;
 import com.password.manager.credentials.services.NoteService;
 
 public class CredentialsFactory {
 
-    public static ICredential getCredentialService(String credentialType) {
-        return switch (credentialType) {
-            case "EMAIL" -> new EmailService();
-            case "CREDITCARD" -> new CreditCardService();
-            case "NOTE" -> new NoteService();
+    public static ICredential getCredentialService(Entity credential) {
+        return switch (credential.getCredentialType()) {
+            case CredentialEnum.EMAIL -> new EmailService();
+            case CredentialEnum.CREDIT_CARD -> new CreditCardService();
+            case CredentialEnum.NOTE -> new NoteService();
             default -> null;
         };
     }
