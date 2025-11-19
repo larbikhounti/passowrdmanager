f1aec786ff3f0034fb3c340b4b10ac62be9c7b12
mohamed khounti <larbikhounti@gmail.com>
2025-11-19 19:08:52 +0100

    feat: refactor DashboardService to utilize RenderService for credential rendering

--- src/main/java/com/password/manager/dashboard/services/DashboardService.java
+++ src/main/java/com/password/manager/dashboard/services/DashboardService.java
@@ -2,30 +2,29 @@ package com.password.manager.dashboard.services;
 
 import com.password.manager.credentials.base.Entity;
 import com.password.manager.credentials.enums.CredentialEnum;
-import com.password.manager.credentials.factories.CredentialsFactory;
+import com.password.manager.credentials.services.RenderService;
 import com.password.manager.utils.Helpers;
-import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class DashboardService {
-    private final VBox credentialsContainer;
     private final Label emailsCredentialCount;
     private final Label notesCredentialCount;
     private final Label creditCardCredentialCount;
     private final Label totalCredentialsCount;
-    public DashboardService(VBox credentialsContainer, Label emailsCredentialCount, Label notesCredentialCount, Label creditCardCredentialCount, Label totalCredentialsCount) {
-        this.credentialsContainer = credentialsContainer;
+    private final RenderService renderService;
+    public DashboardService(VBox credentialsContainer,
+                            VBox credentialContainer,
+                            Label emailsCredentialCount,
+                            Label notesCredentialCount,
+                            Label creditCardCredentialCount,
+                            Label totalCredentialsCount) {
         this.emailsCredentialCount = emailsCredentialCount;
         this.notesCredentialCount = notesCredentialCount;
         this.creditCardCredentialCount = creditCardCredentialCount;
         this.totalCredentialsCount = totalCredentialsCount;
-
+        this.renderService = new RenderService(credentialsContainer, credentialContainer);
     }
 
     public void initializeDashboard() {
@@ -34,11 +33,11 @@ public class DashboardService {
             for (Entity credential : credentials) {
                 switch (credential.getCredentialType()) {
                     case CredentialEnum.EMAIL ->
-                            addCredential(credential.getUrl(), credential.getEmail(), "\uD83D\uDD17");
+                            this.renderService.renderCredentialMany(credential.getId(), credential.getUrl(), credential.getEmail(), "\uD83D\uDD17");
                     case CredentialEnum.NOTE ->
-                            addCredential(credential.getTitle(), credential.getNote(), "\uD83D\uDCDD");
+                            this.renderService.renderCredentialMany(credential.getId(), credential.getTitle(), credential.getNote(), "\uD83D\uDCDD");
                     case CredentialEnum.CREDIT_CARD ->
-                            addCredential(credential.getcreditCardHolderName(), credential.getcreditCardNumber(), "\uD83D\uDCB3");
+                            this.renderService.renderCredentialMany(credential.getId(), credential.getcreditCardHolderName(), credential.getcreditCardNumber(), "\uD83D\uDCB3");
                 }
             }
             emailsCredentialCount.setText(
@@ -69,39 +68,6 @@ public class DashboardService {
         } catch (NullPointerException e) {
             Helpers.Logger("No credential service found: " + e.getMessage(), "ERROR");
         }
-    }
-
-    public void addCredential(String _title, String _subtitle, String _Icon) {
-
-        Helpers.Logger("Adding credential: site=" + _title + ", username=" + _subtitle, "INFO");
-
-        HBox item = new HBox();
-        item.setAlignment(Pos.CENTER_LEFT);
-        item.setSpacing(15);
-        item.setStyle("-fx-background-color: #252525; -fx-padding: 12; -fx-cursor: hand;");
-
-        Label icon = new Label(_Icon);
-        icon.setStyle("-fx-font-size: 24px;");
-
-        VBox textBox = new VBox(2);
-
-        Label title = new Label(_title);
-        title.setStyle("-fx-text-fill: white; -fx-font-size: 14px; -fx-font-weight: bold;");
-
-        Label subtitle = new Label(_subtitle);
-        subtitle.setStyle("-fx-text-fill: #888888; -fx-font-size: 12px;");
-
-        textBox.getChildren().addAll(title, subtitle);
-
-        item.getChildren().addAll(icon, textBox);
-
-
-        // Add item
-        this.credentialsContainer.getChildren().add(item);
-        // Add separator
-        Separator sep = new Separator();
-        sep.setStyle("-fx-background-color: #1e1e1e;");
-        this.credentialsContainer.getChildren().add(sep);
     }
 
 }
