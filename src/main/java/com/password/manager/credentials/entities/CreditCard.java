9a6a83d5874bdb776037e295989b569d44ef30b9
mohamed khounti <larbikhounti@gmail.com>
2025-11-21 18:04:22 +0100

    feat: add CreditCard class for managing credit card credentials with rendering support

---
+++ src/main/java/com/password/manager/credentials/entities/CreditCard.java
@@ -0,0 +1,62 @@
+package com.password.manager.credentials.entities;
+
+import com.password.manager.credentials.base.Entity;
+import com.password.manager.credentials.enums.CredentialEnum;
+import com.password.manager.credentials.services.RenderService;
+
+public class CreditCard  extends Entity {
+    private  String creditCardNumber;
+    private  String creditCardExpiry;
+    private  String creditCardCVV;
+    private  String creditCardHolderName;
+
+    public CreditCard(int id, String creditCardHolderName, String creditCardNumber, String creditCardExpiry, String creditCardCVV) {
+        super(id);
+        this.creditCardHolderName = creditCardHolderName;
+        this.creditCardNumber = creditCardNumber;
+        this.creditCardExpiry = creditCardExpiry;
+        this.creditCardCVV = creditCardCVV;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public String getCreditCardExpiry() {
+        return creditCardExpiry;
+    }
+
+    public void setCreditCardExpiry(String creditCardExpiry) {
+        this.creditCardExpiry = creditCardExpiry;
+    }
+
+    public String getCreditCardCVV() {
+        return creditCardCVV;
+    }
+
+    public void setCreditCardCVV(String creditCardCVV) {
+        this.creditCardCVV = creditCardCVV;
+    }
+
+    public String getCreditCardHolderName() {
+        return creditCardHolderName;
+    }
+
+    public void setCreditCardHolderName(String creditCardHolderName) {
+        this.creditCardHolderName = creditCardHolderName;
+    }
+
+    @Override
+    public void render(RenderService renderer) {
+        renderer.renderCreditCardCredential(this);
+    }
+
+    @Override
+    public void renderMany(RenderService renderer) {
+        renderer.renderCredentialMany(this.getId(), this.creditCardHolderName, this.creditCardNumber,  "[card]");
+    }
+}
