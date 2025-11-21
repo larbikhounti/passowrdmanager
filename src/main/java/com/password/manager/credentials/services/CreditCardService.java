8f42f22d3800204a9ef86f466dfcc9a7d05933b6
mohamed khounti <larbikhounti@gmail.com>
2025-11-21 18:05:19 +0100

    feat: refactor CreditCardService to use static credentials and improve logging for credit card details

--- src/main/java/com/password/manager/credentials/services/CreditCardService.java
+++ src/main/java/com/password/manager/credentials/services/CreditCardService.java
@@ -2,21 +2,24 @@ package com.password.manager.credentials.services;
 
 import com.password.manager.credentials.base.Entity;
 import com.password.manager.credentials.contracts.ICredential;
+import com.password.manager.credentials.entities.CreditCard;
 
 import java.util.ArrayList;
 
 public class CreditCardService implements ICredential {
-    private final ArrayList<Entity> _credentials;
 
     public CreditCardService() {
-        _credentials = Entity.getCredentials();
     }
     @Override
     public boolean addCredential(Entity credential) {
-        boolean result =  _credentials.add(credential);
-        for (Entity c : _credentials) {
+        boolean result =  Entity.credentials.add(credential);
+        for (Entity c : Entity.credentials) {
+            if (c == null) continue;
+            if (!(c instanceof CreditCard creditCard)) {
+                continue;
+            }
             System.out.printf("id is %d creditCard is %s creditCardHolderName is %s creditCardExpiry is %s creditCardCVV is %s \n",
-                    c.getId(), c.getcreditCardNumber(), c.getcreditCardHolderName(), c.getcreditCardExpiry(), c.getcreditCardCVV());
+                    c.getId(), creditCard.getId(), creditCard.getCreditCardHolderName(), creditCard.getCreditCardExpiry(), creditCard.getCreditCardCVV());
         }
         return result;
     }
