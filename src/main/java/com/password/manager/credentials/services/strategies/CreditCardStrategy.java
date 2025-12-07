5472b72c7fd133fafa0ad0ba9b20b29130836e0f
mohamed khounti <larbikhounti@gmail.com>
2025-12-07 01:49:29 +0100

    feat: integrate CreditCardRepository into CreditCardStrategy for credential management

--- src/main/java/com/password/manager/credentials/services/strategies/CreditCardStrategy.java
+++ src/main/java/com/password/manager/credentials/services/strategies/CreditCardStrategy.java
@@ -3,44 +3,38 @@ package com.password.manager.credentials.services.strategies;
 import com.password.manager.credentials.base.Entity;
 import com.password.manager.credentials.contracts.ICredential;
 import com.password.manager.credentials.entities.CreditCard;
+import com.password.manager.credentials.repositories.CreditCardRepository;
 
 import java.util.ArrayList;
 
 public class CreditCardStrategy implements ICredential {
+    CreditCardRepository creditCardRepository;
 
     public CreditCardStrategy() {
+        this.creditCardRepository = new CreditCardRepository();
     }
     @Override
     public boolean addCredential(Entity credential) {
-        boolean result =  Entity.credentials.add(credential);
-        for (Entity c : Entity.credentials) {
-            if (c == null) continue;
-            if (!(c instanceof CreditCard creditCard)) {
-                continue;
-            }
-            System.out.printf("id is %d creditCard is %s creditCardHolderName is %s creditCardExpiry is %s creditCardCVV is %s \n",
-                    c.getId(), creditCard.getId(), creditCard.getCreditCardHolderName(), creditCard.getCreditCardExpiry(), creditCard.getCreditCardCVV());
-        }
-        return result;
+        return this.creditCardRepository.addCredential(credential);
     }
 
     @Override
     public boolean editCredential(int id, Entity credential) {
-        return false;
+        return this.creditCardRepository.editCredential(id, credential);
     }
 
     @Override
     public boolean removeCredential(int id) {
-        return false;
+        return this.creditCardRepository.removeCredential(id);
     }
 
     @Override
     public Entity getCredential(int id) {
-        return null;
+        return this.creditCardRepository.getCredential(id);
     }
 
     @Override
     public ArrayList<Entity> getAllCredentials() {
-        return null;
+        return this.creditCardRepository.getAllCredentials();
     }
 }
