0974aba58b9b27f70086e04ef6de02a570236340
mohamed khounti <larbikhounti@gmail.com>
2025-12-07 01:49:38 +0100

    feat: integrate DashboardRepository into DashboardService for credential management

--- src/main/java/com/password/manager/dashboard/services/DashboardService.java
+++ src/main/java/com/password/manager/dashboard/services/DashboardService.java
@@ -6,9 +6,12 @@ import com.password.manager.credentials.entities.Email;
 import com.password.manager.credentials.entities.Note;
 import com.password.manager.credentials.enums.CredentialEnum;
 import com.password.manager.credentials.services.RenderService;
+import com.password.manager.dashboard.repositories.DashboardRepository;
 import com.password.manager.utils.Helpers;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+
+import java.sql.Connection;
 import java.util.ArrayList;
 
 public class DashboardService {
@@ -17,6 +20,8 @@ public class DashboardService {
     private final Label creditCardCredentialCount;
     private final Label totalCredentialsCount;
     private final RenderService renderService;
+    private DashboardRepository dashboardRepository;
+    private ArrayList<Entity> credentials;
     public DashboardService(VBox credentialsContainer,
                             VBox credentialContainer,
                             Label emailsCredentialCount,
@@ -28,23 +33,26 @@ public class DashboardService {
         this.creditCardCredentialCount = creditCardCredentialCount;
         this.totalCredentialsCount = totalCredentialsCount;
         this.renderService = new RenderService(credentialsContainer, credentialContainer);
+        this.dashboardRepository = new DashboardRepository();
+
     }
 
     public void initializeDashboard() {
         try {
-            for (Entity credential : Entity.credentials) {
+            this.credentials = this.dashboardRepository.getAllCredentials();
+            for (Entity credential : this.credentials) {
                 credential.renderMany(renderService);
             }
             emailsCredentialCount.setText(
                     String.valueOf(
-                            Entity.credentials.stream()
+                            this.credentials.stream()
                                     .filter(c -> c instanceof Email)
                                     .count()
                     )
             );
             notesCredentialCount.setText(
                     String.valueOf(
-                            Entity.credentials.stream()
+                            this.credentials.stream()
                                     .filter(c -> c instanceof Note)
                                     .count()
                     )
@@ -52,13 +60,13 @@ public class DashboardService {
 
             creditCardCredentialCount.setText(
                     String.valueOf(
-                            Entity.credentials.stream()
+                            this.credentials.stream()
                                     .filter(c -> c instanceof CreditCard)
                                     .count()
                     )
             );
 
-            totalCredentialsCount.setText(String.valueOf(Entity.credentials.size()));
+            totalCredentialsCount.setText(String.valueOf(this.credentials.size()));
 
         } catch (NullPointerException e) {
             Helpers.Logger("No credential service found: " + e.getMessage(), "ERROR");
