8c381f02cbc8ee4c5316a6685348dea7bc80217e
mohamed khounti <larbikhounti@gmail.com>
2025-12-07 01:50:04 +0100

    feat: add DbConnector class for managing database connections

---
+++ src/main/java/com/password/manager/utils/DbConnector.java
@@ -0,0 +1,49 @@
+package com.password.manager.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnector  {
+   static Connection connection = null;
+
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            if (connection == null || connection.isClosed()) {
+                createConnection();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+    private static void createConnection() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@//192.168.1.8:1521/xe",
+                "system", "123456");
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+    public static boolean checkConnection() {
+        try {
+            Connection conn = DbConnector.getConnection();
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Database connection is active.");
+                return true;
+            } else {
+
+                System.out.println("Database connection is not active.");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error checking database connection: " + e.getMessage());
+        }
+        return false;
+    }
+}
